2e73216cf0ff28d556a09edc4839dc4198c1f688
Lari Hotari <lari@hotari.net>
2016-09-08 16:01:15 +0300

    Add init.gradle

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
+export ENTERPRISE_REPOSITORY_URL="file:$PWD/customPlugin/repo"
+export INITPLUGIN_REPOSITORY_URL="file:$PWD/initPlugin/repo"
 echo "Gradle version:"
 gradle --version
 set -x
 cd basic-composite/composite
-gradle -S helloAll
+gradle -I init.gradle -S helloAll
