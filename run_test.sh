b63083891f223a45a5cd0d01f6d24380c3978e19
Lari Hotari <lari@hotari.net>
2016-09-08 16:31:28 +0300

    Test applying init script
    - composite builds don't support specifying init script from command
      line

--- run_test.sh
+++ run_test.sh
@@ -2,8 +2,9 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 export ENTERPRISE_REPOSITORY_URL="file:$PWD/customPlugin/repo"
 export INITPLUGIN_REPOSITORY_URL="file:$PWD/initPlugin/repo"
+export GRADLE_USER_HOME="$PWD/gradle-user-home"
 echo "Gradle version:"
 gradle --version
 set -x
 cd basic-composite/composite
-gradle -I init.gradle -S helloAll
+gradle -g $GRADLE_USER_HOME -S helloAll
