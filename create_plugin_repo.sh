faef2702342812b4af2910b96114a1a542575a8a
Lari Hotari <lari@hotari.net>
2016-09-08 13:49:33 +0300

    Publish a few versions in the generated plugin repo

--- create_plugin_repo.sh
+++ create_plugin_repo.sh
@@ -4,5 +4,8 @@ for i in plugin plugin2; do
     (
     cd $i
     gradle uploadArchives
+    for version in 1.0 1.1 1.2; do
+        gradle -PversionAsParameter=$version uploadArchives
+    done
     )
 done
