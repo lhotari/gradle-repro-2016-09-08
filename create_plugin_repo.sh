962eb2510ae2e8df6c4d6196bb7b38201c41f4c2
Lari Hotari <lari@hotari.net>
2016-09-08 15:18:27 +0300

    Add init plugin

--- create_plugin_repo.sh
+++ create_plugin_repo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-cd customPlugin
-for i in plugin plugin2; do
+for i in customPlugin/plugin customPlugin/plugin2 initPlugin/plugin; do
     (
     cd $i
+    echo $PWD
     gradle uploadArchives
     for version in 1.0 1.1 1.2; do
         gradle -PversionAsParameter=$version uploadArchives
---
+++ initPlugin/plugin/src/main/groovy/org/gradle/EnterpriseRepositoryPlugin.groovy
@@ -0,0 +1,31 @@
+package org.gradle
+
+import org.gradle.api.*
+import org.gradle.api.invocation.Gradle
+import org.gradle.api.artifacts.repositories.ArtifactRepository
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+
+class EnterpriseRepositoryPlugin implements Plugin<Gradle> {
+    private static String ENTERPRISE_REPOSITORY_URL = System.env('ENTERPRISE_REPOSITORY_URL') ?: "https://repo.gradle.org/gradle/repo"
+
+    void apply(Gradle gradle) {
+        def allowedUrls = [ENTERPRISE_REPOSITORY_URL, 'https://jcenter.bintray.com/']
+        gradle.allprojects{ project ->
+            project.repositories {
+                all { ArtifactRepository repo ->
+                    if (!(repo instanceof MavenArtifactRepository) ||
+                          !(repo.url.toString() in allowedUrls)) {
+                        project.logger.lifecycle "Repository ${repo.url} removed. Only $ENTERPRISE_REPOSITORY_URL or jcenter() is allowed"
+                        remove repo
+                    }
+                }
+
+                maven {
+                    name "STANDARD_ENTERPRISE_REPO"
+                    url ENTERPRISE_REPOSITORY_URL
+                }
+                jcenter()
+            }
+        }
+    }
+}
